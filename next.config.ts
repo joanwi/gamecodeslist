496ba1782642aa5dfa69a5cfbbef46a8a531428b
joanwi <willyuan007@gmail.com>
2025-03-06 13:40:26 +0800

    nextconfg

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,23 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  // output: 'export',
+  swcMinify: false,
+  experimental: {
+    optimizeCss: false,
+  },
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
+  images: {
+    unoptimized: true,
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'images.unsplash.com',
+      },
+    ],
+  },
 };
 
 export default nextConfig;
